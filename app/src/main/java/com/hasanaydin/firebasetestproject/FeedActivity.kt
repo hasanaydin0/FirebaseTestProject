75e17106541fc19302eeb4248db77845ef3dee36
hasanaydin0 <84991976+hasanaydin0@users.noreply.github.com>
2021-06-30 22:05:58 +0300

    OptionsMenu added

--- app/src/main/java/com/hasanaydin/firebasetestproject/FeedActivity.kt
+++ app/src/main/java/com/hasanaydin/firebasetestproject/FeedActivity.kt
@@ -1,11 +1,59 @@
 package com.hasanaydin.firebasetestproject
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuInflater
+import android.view.MenuItem
+import com.google.firebase.auth.FirebaseAuth
+import com.hasanaydin.firebasetestproject.databinding.ActivityFeedBinding
 
 class FeedActivity : AppCompatActivity() {
+
+    private lateinit var binding : ActivityFeedBinding
+
+    private lateinit var auth : FirebaseAuth
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_feed)
+        binding = ActivityFeedBinding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)
+
+        auth = FirebaseAuth.getInstance()
+
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+
+        val menuInflater = menuInflater
+        menuInflater.inflate(R.menu.options_menu,menu)
+
+        return super.onCreateOptionsMenu(menu)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+
+        if (item.itemId == R.id.add_post){
+
+            // Upload Activity
+
+            val intent = Intent(applicationContext,UploadActivity::class.java)
+            startActivity(intent)
+            // finish()
+
+        }else if (item.itemId == R.id.logout){
+
+            // Logout
+
+            auth.signOut()
+            val intent = Intent(applicationContext,MainActivity::class.java)
+            startActivity(intent)
+            finish()
+
+        }
+
+        return super.onOptionsItemSelected(item)
     }
 }
