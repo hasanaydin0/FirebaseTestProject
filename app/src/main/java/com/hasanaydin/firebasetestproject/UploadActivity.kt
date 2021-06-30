313f4a90c389ad27a2ce4e1d02864fd2c9e89249
hasanaydin0 <84991976+hasanaydin0@users.noreply.github.com>
2021-06-30 23:24:51 +0300

    Select-Picture

--- app/src/main/java/com/hasanaydin/firebasetestproject/UploadActivity.kt
+++ app/src/main/java/com/hasanaydin/firebasetestproject/UploadActivity.kt
@@ -1,11 +1,90 @@
 package com.hasanaydin.firebasetestproject
 
+import android.content.Intent
+import android.content.pm.PackageManager
+import android.graphics.ImageDecoder
+import android.net.Uri
+import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.provider.MediaStore
+import android.view.View
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
+import com.hasanaydin.firebasetestproject.databinding.ActivityUploadBinding
+
 
 class UploadActivity : AppCompatActivity() {
+
+    private lateinit var binding : ActivityUploadBinding
+
+    var selectedPicture : Uri? = null
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_upload)
+        binding = ActivityUploadBinding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)
+    }
+
+
+    fun imageViewClicked(view : View){
+
+        if(ContextCompat.checkSelfPermission(this,android.Manifest.permission.READ_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED){
+            ActivityCompat.requestPermissions(this, arrayOf(android.Manifest.permission.READ_EXTERNAL_STORAGE),1)
+        }else{
+            val intent = Intent(Intent.ACTION_PICK,MediaStore.Images.Media.EXTERNAL_CONTENT_URI)
+            startActivityForResult(intent,2)
+        }
+
+    }
+
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<out String>,
+        grantResults: IntArray
+    ) {
+
+        if (requestCode == 1){
+            if (grantResults.size > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED){
+                val intent = Intent(Intent.ACTION_PICK,MediaStore.Images.Media.EXTERNAL_CONTENT_URI)
+                startActivityForResult(intent,2)
+            }
+        }
+
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+
+        if (requestCode == 2 && resultCode == RESULT_OK && data != null){
+
+            selectedPicture = data.data
+
+            try {
+                if (selectedPicture != null) {
+
+                    if (Build.VERSION.SDK_INT >= 28) {
+                        val source = ImageDecoder.createSource(contentResolver, selectedPicture!!)
+                        val bitmap = ImageDecoder.decodeBitmap(source)
+                        binding.uploadImageView.setImageBitmap(bitmap)
+                    } else {
+                        val bitmap =
+                            MediaStore.Images.Media.getBitmap(this.contentResolver, selectedPicture)
+                        binding.uploadImageView.setImageBitmap(bitmap)
+                    }
+
+                }
+            }catch (e:Exception){
+                e.printStackTrace()
+            }
+
+        }
+
+        super.onActivityResult(requestCode, resultCode, data)
+    }
+
+    fun uploadClicked (view: View){
+
     }
 }
